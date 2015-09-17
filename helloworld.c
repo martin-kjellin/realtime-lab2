1a187b65a9dced9503ee604d23748d5cfa5faf51
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-17 14:54:26 +0200

    Added basic code.

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+
+DeclareTask(HelloworldTask);
+
+void ecrobot_device_initialize() {}
+void ecrobot_device_terminate() {}
+void user_1ms_isr_type2(void) {}
+
+TASK(HelloworldTask)
+{
+
+    // Your code comes here
+
+    TerminateTask();
+}
