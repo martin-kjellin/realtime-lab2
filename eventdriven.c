cd8025c04e3ef419d293efdb8f52dac4c194a1f9
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-22 16:27:30 +0200

    Light sensor not really working yet.

--- eventdriven.c
+++ eventdriven.c
@@ -9,12 +9,19 @@ DeclareTask(EventdispatcherTask);
 DeclareEvent(TouchOnEvent);
 DeclareEvent(TouchOffEvent);
 
+DeclareEvent(OnTableEvent);
+DeclareEvent(OffTableEvent);
+
 void ecrobot_device_initialize() {
+  ecrobot_set_light_sensor_active(NXT_PORT_S1);
+
   nxt_motor_set_speed(NXT_PORT_A, 0, 1);
   nxt_motor_set_speed(NXT_PORT_B, 0, 1);
 }
 
 void ecrobot_device_terminate() {
+  ecrobot_set_light_sensor_inactive(NXT_PORT_S1);
+
   nxt_motor_set_speed(NXT_PORT_A, 0, 1);
   nxt_motor_set_speed(NXT_PORT_B, 0, 1);
 }
@@ -30,6 +37,8 @@ TASK(MotorcontrolTask) {
   while (TRUE) {
     WaitEvent(TouchOnEvent);
     ClearEvent(TouchOnEvent);
+    WaitEvent(OnTableEvent);
+    ClearEvent(OnTableEvent);
     display_clear(1);
     display_goto_xy(0, 0);
     display_string("Motors on");
@@ -37,8 +46,9 @@ TASK(MotorcontrolTask) {
     nxt_motor_set_speed(NXT_PORT_A, 50, 1);
     nxt_motor_set_speed(NXT_PORT_B, 50, 1);
 
-    WaitEvent(TouchOffEvent);
+    WaitEvent(TouchOffEvent | OffTableEvent);
     ClearEvent(TouchOffEvent);
+    ClearEvent(OffTableEvent);
     display_clear(1);
     display_goto_xy(0, 0);
     display_string("Motors off");
@@ -52,6 +62,7 @@ TASK(MotorcontrolTask) {
 
 TASK(EventdispatcherTask) {
   int status = 0;
+  int on_table = 0;
 
   while (TRUE) {
     if (status == 0 && ecrobot_get_touch_sensor(NXT_PORT_S3) == 1) {
@@ -62,6 +73,16 @@ TASK(EventdispatcherTask) {
     if (status == 1 && ecrobot_get_touch_sensor(NXT_PORT_S3) == 0) {
       SetEvent(MotorcontrolTask, TouchOffEvent);
       status = 0;
+    }
+
+    if (on_table == 0 && ecrobot_get_light_sensor(NXT_PORT_S1) < 700) {
+      SetEvent(MotorcontrolTask, OnTableEvent);
+      on_table = 1;
+    }
+
+    if (on_table == 1 && ecrobot_get_light_sensor(NXT_PORT_S1) >= 700) {
+      SetEvent(MotorcontrolTask, OffTableEvent);
+      on_table = 0;
     }
 
     systick_wait_ms(10);
