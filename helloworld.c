85e5de96a48c76d3458ed55dbb7f0fec6670f384
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-22 14:22:09 +0200

    Final version of the hello word program.

--- helloworld.c
+++ helloworld.c
@@ -5,14 +5,31 @@
 
 DeclareTask(HelloworldTask);
 
-void ecrobot_device_initialize() {}
-void ecrobot_device_terminate() {}
+void ecrobot_device_initialize() {
+  ecrobot_set_light_sensor_active(NXT_PORT_S1);
+}
+
+void ecrobot_device_terminate() {
+  ecrobot_set_light_sensor_inactive(NXT_PORT_S1);
+}
+
 void user_1ms_isr_type2(void) {}
 
-TASK(HelloworldTask)
-{
+TASK(HelloworldTask) {
+  display_string("Hello World!");
+  display_update();
+  systick_wait_ms(3000);
 
-    // Your code comes here
+  int reading = 0;
 
-    TerminateTask();
+  while (TRUE) {
+    reading = ecrobot_get_light_sensor(NXT_PORT_S1);
+    display_clear(1);
+    display_goto_xy(0, 0);
+    display_int(reading, 4);
+    display_update();
+    systick_wait_ms(100);
+  }
+
+  TerminateTask();
 }
