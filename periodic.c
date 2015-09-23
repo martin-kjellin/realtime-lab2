2824c941e47e360b8db111888c120a5e4a233015
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-23 08:49:20 +0200

    Added some more given information for task 3.

--- periodic.c
+++ periodic.c
@@ -3,12 +3,29 @@
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
 
+#define PRIO_IDLE 10
+#define PRIO_BUTTON 20
+
+DeclareCounter(SysTimerCnt);
+
+struct dc_t {
+    U32 duration;
+    S32 speed;
+    int priority;
+} dc = {0, 0, PRIO_IDLE};
+
 void ecrobot_device_initialize() {
-  ecrobot_set_light_sensor_active(NXT_PORT_S1);
+  //  ecrobot_set_light_sensor_active(NXT_PORT_S1);
 }
 
 void ecrobot_device_terminate() {
-  ecrobot_set_light_sensor_inactive(NXT_PORT_S1);
+  //  ecrobot_set_light_sensor_inactive(NXT_PORT_S1);
 }
 
-void user_1ms_isr_type2(void) {}
+void user_1ms_isr_type2(void){
+  (void) SignalCounter(SysTimerCnt);
+}
+
+void change_driving_command(int priority, int speed, int duration) {
+
+}
