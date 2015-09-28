23df98b3aa7f7b42234fbfde0e0c6a4a83302da6
Martin Kjellin <martin.kjellin.3271@student.uu.se>
2015-09-28 13:00:23 +0200

    Final version of code and report.

--- periodic.c
+++ periodic.c
@@ -8,105 +8,134 @@
 #define PRIO_BUTTON 20
 
 DeclareCounter(SysTimerCnt);
+
 DeclareTask(MotorcontrolTask);
-DeclareTask(ButtonPressTask);
+DeclareTask(ButtonpressTask);
 DeclareTask(DisplayTask);
 DeclareTask(DistanceTask);
+
 DeclareResource(resource_dc);
 
+/* Structure storing driving commands from the sensing tasks. */
 struct dc_t {
-    U32 duration;
-    S32 speed;
-    int priority;
+  U32 duration;
+  S32 speed;
+  int priority;
 } dc = {0, 0, PRIO_IDLE};
 
 void ecrobot_device_initialize() {
-  //  ecrobot_set_light_sensor_active(NXT_PORT_S1);
   ecrobot_init_sonar_sensor(NXT_PORT_S2);
 }
 
 void ecrobot_device_terminate() {
-  //  ecrobot_set_light_sensor_inactive(NXT_PORT_S1);
   ecrobot_term_sonar_sensor(NXT_PORT_S2);
 }
-
 
 void user_1ms_isr_type2(void){
   (void) SignalCounter(SysTimerCnt);
 }
 
+/* Stores a new driving command, if it has a higher priority than
+   the current one. */
 void change_driving_command(int priority, int speed, int duration) {
-	
-	GetResource(resource_dc);
-	if(priority >= dc.priority)
-	{
-		dc.speed=speed;
-		dc.duration=duration;
-		dc.priority=priority;
-	}
-	ReleaseResource(resource_dc);
-}
+  GetResource(resource_dc);
 
-TASK (MotorcontrolTask){
-
-	GetResource(resource_dc);
-	if(dc.duration > 0) 
-	{
-		nxt_motor_set_speed(NXT_PORT_A, dc.speed, 1);
-		nxt_motor_set_speed(NXT_PORT_B, dc.speed, 1);
-		dc.duration-=50;
-		//change_driving_command();
-	}
-	else
-	{
-		nxt_motor_set_speed(NXT_PORT_A, 0, 1);
-		nxt_motor_set_speed(NXT_PORT_B, 0, 1);
-		dc.priority = PRIO_IDLE;
-	}
-	ReleaseResource(resource_dc);
-	TerminateTask();
-}
-
-TASK (ButtonPressTask){
-
-	if(ecrobot_get_touch_sensor(NXT_PORT_S3) == 1)
-	{
-            change_driving_command(PRIO_BUTTON, -50, 1000);
-	}
-	TerminateTask();
-}
-
-TASK (DistanceTask){
-    int dis = ecrobot_get_sonar_sensor(NXT_PORT_S2);
-    if(dis >= 0)
+  if (priority >= dc.priority)
     {
-      dis-=4;
-      int speed = (dis-20)*3;
-      if(speed > 50) speed = 50;
-      else if(speed < 10 && speed > 0) speed=10;
-      else if(speed > -10 && speed < 0) speed=-10;
-      else if(speed < -50 ) speed = -50;
+      dc.speed = speed;
+      dc.duration = duration;
+      dc.priority = priority;
+    }
+
+  ReleaseResource(resource_dc);
+}
+
+/* Sets the speed of the motors according to the stored
+   driving command, if the time of the command has not
+   elapsed. Otherwise, sets the priority of the driving
+   command struct to the lowest possible, in order to
+   prepare it for receiving a new command. */
+TASK(MotorcontrolTask) {
+  GetResource(resource_dc);
+
+  if (dc.duration > 0) 
+    {
+      nxt_motor_set_speed(NXT_PORT_A, dc.speed, 1);
+      nxt_motor_set_speed(NXT_PORT_B, dc.speed, 1);
+      dc.duration -= 50;
+    }
+  else
+    {
+      nxt_motor_set_speed(NXT_PORT_A, 0, 1);
+      nxt_motor_set_speed(NXT_PORT_B, 0, 1);
+      dc.priority = PRIO_IDLE;
+    }
+
+  ReleaseResource(resource_dc);
+
+  TerminateTask();
+}
+
+/* Stores a new driving command making the car go
+   backwards, if the touch sensor is pressed. */
+TASK(ButtonpressTask) {
+  if (ecrobot_get_touch_sensor(NXT_PORT_S3) == 1)
+    {
+      change_driving_command(PRIO_BUTTON, -50, 1000);
+    }
+  TerminateTask();
+}
+
+/* Displays information about the current driving commands
+   and the current distance. */
+TASK(DisplayTask) {
+  GetResource(resource_dc);
+
+  display_clear(1);
+  display_goto_xy(0, 0);
+
+  display_string("dc.priority: ");
+  display_int(dc.priority, 2);
+
+  display_string("\ndc.speed: ");
+  display_int(dc.speed, 4);
+
+  display_string("\ndc.dura: ");
+  display_int(dc.duration, 4);
+
+  /* Displays the current distance reading, compensating for
+     inaccuracies. */
+  display_string("\ndistance: ");
+  display_int(ecrobot_get_sonar_sensor(NXT_PORT_S2) - 4, 3);
+
+  display_update();
+
+  ReleaseResource(resource_dc);
+
+  TerminateTask();
+}
+
+/* Keeps a constant distance of 20 cm to another object. */
+TASK(DistanceTask) {
+  int dis = ecrobot_get_sonar_sensor(NXT_PORT_S2);
+
+  if (dis >= 0)
+    {
+      /* Compensate for inaccuracies in measurements. */
+      dis -= 4;
+
+      /* Set speed and direction according to the distance
+         to the object. */
+      int speed = (dis - 20) * 3;
+
+      /* Keep the speed within certain limits. */
+      if (speed > 50) speed = 50;
+      else if (speed < 10 && speed > 0) speed= 10;
+      else if (speed > -10 && speed < 0) speed= -10;
+      else if (speed < -50 ) speed = -50;
      
       change_driving_command(PRIO_DIST, speed, 100);
     }
-    TerminateTask();
-}
 
-TASK (DisplayTask){
-
-    GetResource(resource_dc);
-    display_clear(1);
-    display_goto_xy(0, 0);
-    display_string("dc.priority: ");
-    display_int(dc.priority, 2);
-    display_string("\ndc.speed: ");
-    display_int(dc.speed, 4);
-    display_string("\ndc.dura: ");
-    display_int(dc.duration, 4);
-    display_string("\ndis: ");
-    display_int(ecrobot_get_sonar_sensor(NXT_PORT_S2)-4, 3);
-    display_update();
-    ReleaseResource(resource_dc);
-
-    TerminateTask();
+  TerminateTask();
 }
--- race.c
+++ race.c
@@ -10,7 +10,7 @@
 
 int colormax = 690;
 int colormin = 0;
-int colormiddle=0;
+int colormiddle = 0;
 
 int speedmax = 50;
 int speedmin = 10;
@@ -28,8 +28,10 @@ DeclareTask(DisplayTask);
 DeclareTask(DistanceTask);
 DeclareTask(MoveStraightTask);
 DeclareTask(DistanceTask);
+
 DeclareResource(resource_dc);
 
+/* Structure storing driving commands from the sensing tasks. */
 struct dc_t {
   U32 duration;
   S32 speedleft;
@@ -47,33 +49,39 @@ void ecrobot_device_terminate() {
   ecrobot_term_sonar_sensor(NXT_PORT_S2);
 }
 
-
 void user_1ms_isr_type2(void){
   (void) SignalCounter(SysTimerCnt);
 }
 
+/* Stores a new driving command, if it has a higher priority than
+   the current one. */
 void change_driving_command(int priority, int speedleft, int speedright, int duration) {
-	
   GetResource(resource_dc);
-  if(priority >= dc.priority)
+
+  if (priority >= dc.priority)
     {
-      dc.speedleft=speedleft;
-      dc.speedright=speedright;
-      dc.duration=duration;
-      dc.priority=priority;
+      dc.speedleft = speedleft;
+      dc.speedright = speedright;
+      dc.duration = duration;
+      dc.priority = priority;
     }
+
   ReleaseResource(resource_dc);
 }
 
+/* Sets the speed of the motors according to the stored
+   driving command, if the time of the command has not
+   elapsed. Otherwise, sets the priority of the driving
+   command struct to the lowest possible, in order to
+   prepare it for receiving a new command. */
 TASK (MotorcontrolTask){
-
   GetResource(resource_dc);
-  if(dc.duration > 0) 
+
+  if (dc.duration > 0) 
     {
       nxt_motor_set_speed(NXT_PORT_A, dc.speedright, 1);
       nxt_motor_set_speed(NXT_PORT_B, dc.speedleft, 1);
-      dc.duration-=50;
-     
+      dc.duration -= 50;
     }
   else
     {
@@ -81,80 +89,98 @@ TASK (MotorcontrolTask){
       nxt_motor_set_speed(NXT_PORT_B, 0, 1);
       dc.priority = PRIO_IDLE;
     }
+
   ReleaseResource(resource_dc);
+
   TerminateTask();
 }
 
+/* Calibrates the light sensor when the touch sensor is pressed twice. */
 TASK (ButtonPressTask){
-  if(ecrobot_get_touch_sensor(NXT_PORT_S3) == 1)
+  if (ecrobot_get_touch_sensor(NXT_PORT_S3) == 1)
     {
-      if(counter == 0){
-	colormax = ecrobot_get_light_sensor(NXT_PORT_S1);
-	counter=1;
-      }
-
-      else {
-	colormin = ecrobot_get_light_sensor(NXT_PORT_S1);
-	counter=0;
-	colormiddle = (colormax+colormin)/2;
-      }
-     
+      if (counter == 0)
+        {
+	  colormax = ecrobot_get_light_sensor(NXT_PORT_S1);
+	  counter = 1;
+        }
+      else
+        {
+	  colormin = ecrobot_get_light_sensor(NXT_PORT_S1);
+	  counter = 0;
+	  colormiddle = (colormax + colormin) / 2;
+        }
     }
 
   TerminateTask();
 }
 
-TASK (MoveStraightTask){
-  
+/* Makes the car follow a line on the floor. */
+TASK(MoveStraightTask){
   int adjustnum=8;
   int color = ecrobot_get_light_sensor(NXT_PORT_S1);
-  right_speed = adjustnum*(color-colormiddle)+50;
+  right_speed = adjustnum * (color - colormiddle) + 50;
   left_speed = -right_speed;
-  if(right_speed > speedmax) right_speed = speedmax;
-  if(right_speed < speedmin) right_speed = speedmin;
-  if(left_speed > speedmax) left_speed = speedmax;
-  if(left_speed < speedmin) left_speed = speedmin;
+  if (right_speed > speedmax) right_speed = speedmax;
+  if (right_speed < speedmin) right_speed = speedmin;
+  if (left_speed > speedmax) left_speed = speedmax;
+  if (left_speed < speedmin) left_speed = speedmin;
 
   change_driving_command(PRIO_DIST, left_speed, right_speed, duration);
   TerminateTask();
 
 }
 
-TASK (DistanceTask){
-  int dis = ecrobot_get_sonar_sensor(NXT_PORT_S2);
-  if(dis >= 0)
-    {
-      dis-=4;
-      if(dis <=40){
-	change_driving_command(PRIO_DIST, 0, 0, 100);
-      }
+/* Keeps a constant distance to another object. */
+TASK(DistanceTask){
 
+  int dis = ecrobot_get_sonar_sensor(NXT_PORT_S2);
+
+  if (dis >= 0)
+    {
+      dis -= 4;
+
+      if (dis <= 40) /* 40 cm since the sensor is mounted at the back. */
+        {
+	  change_driving_command(PRIO_DIST, 0, 0, 100);
+        }
     }
   TerminateTask();
-  }
+}
 
-TASK (DisplayTask){
-
+/* Displays information about the current status. */
+TASK(DisplayTask){
   GetResource(resource_dc);
+
   display_clear(1);
   display_goto_xy(0, 0);
+
   display_string("color: ");
   display_int(ecrobot_get_light_sensor(NXT_PORT_S1), 4);
+
   display_string("\ndc.priority: ");
   display_int(dc.priority, 2);
+
   display_string("\nlspeed: ");
   display_int(dc.speedleft, 4);
+
   display_string("\nrspeed: ");
   display_int(dc.speedright, 4);
+
   display_string("\ndc.dura: ");
   display_int(dc.duration, 4);
+
   display_string("\ndis: ");
   display_int(ecrobot_get_sonar_sensor(NXT_PORT_S2)-4, 3);
+
   display_string("\ncmax: ");
   display_int(colormax, 4);
+
   display_string("\ncmin: ");
   display_int(colormin, 4);
+
   display_update();
+
   ReleaseResource(resource_dc);
 
   TerminateTask();
